4f45b7c5fbfb28da43421aba9066762c462b540a
sungmin <kimmoksil@gmail.com>
2023-05-03 10:13:58 +0900

    [feat] (back) admin용 stuffAll api 추가

--- BackEnd_Modeul/src/main/java/com/modeul/web/service/StuffServiceImpl.java
+++ BackEnd_Modeul/src/main/java/com/modeul/web/service/StuffServiceImpl.java
@@ -32,7 +32,7 @@ public class StuffServiceImpl implements StuffService {
 	@Override
 	public List<StuffView> getViewAll() {
 
-		return repository.findViewAll("", null, null, null, 10, 0, null);
+		return repository.findViewAll(null, null, "reg_date", "desc", null, null, null);
 	}
 
 	@Override
