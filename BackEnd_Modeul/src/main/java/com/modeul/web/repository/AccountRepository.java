9b63ff18d5eb702066bfbafe520f86b7829bcc79
Mistercandoit <simma91@naver.com>
2023-05-10 12:41:42 +0900

    dev2 hyeon 병합

--- BackEnd_Modeul/src/main/java/com/modeul/web/repository/AccountRepository.java
+++ BackEnd_Modeul/src/main/java/com/modeul/web/repository/AccountRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.modeul.web.entity.Account;
-import com.modeul.web.entity.AccountView;
 
 @Mapper
 public interface AccountRepository {
