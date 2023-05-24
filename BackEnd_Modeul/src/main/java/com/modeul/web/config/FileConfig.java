f234e709191de4ca15c48bc4c22ed775b71704f2
sungmin <kimmoksil@gmail.com>
2023-05-24 18:29:32 +0900

    [fix] 경로설정파일 주석설정

--- BackEnd_Modeul/src/main/java/com/modeul/web/config/FileConfig.java
+++ BackEnd_Modeul/src/main/java/com/modeul/web/config/FileConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+// @Configuration
 public class FileConfig implements WebMvcConfigurer {
  
     @Override
