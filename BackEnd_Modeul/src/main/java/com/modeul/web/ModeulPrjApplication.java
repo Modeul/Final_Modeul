642188ada08928006923544b38f56389c84e1e5c
sungmin <kimmoksil@gmail.com>
2023-05-25 12:02:37 +0900

    war 배포설정 추가

--- BackEnd_Modeul/src/main/java/com/modeul/web/ModeulPrjApplication.java
+++ BackEnd_Modeul/src/main/java/com/modeul/web/ModeulPrjApplication.java
@@ -2,9 +2,16 @@ package com.modeul.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ModeulPrjApplication {
+public class ModeulPrjApplication extends SpringBootServletInitializer {
+
+@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	return builder.sources(ModeulPrjApplication.class);
+}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModeulPrjApplication.class, args);
